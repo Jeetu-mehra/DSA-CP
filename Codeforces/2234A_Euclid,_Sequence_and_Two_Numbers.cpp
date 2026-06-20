cb8e450c66f0a92db468c12b3304837e6d6f7db1
Jeetu Mehra <jeetumehramtr@gmail.com>
2026-06-20 23:12:11 +0530

    CF 2234A

---
+++ Codeforces/2234A_Euclid,_Sequence_and_Two_Numbers.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int n;
+        cin>>n;
+        vector<int> b(n);
+        for(int i=0;i<n;i++){
+            cin>>b[i];
+        }
+        sort(b.begin(),b.end(),[](int a,int b){
+            return a>b;
+        });
+        vector<int> a{b[0],b[1]};
+        int k=n-2;
+        int i=0;
+        while(k--){
+            if(a[i]%a[i+1]!=0){
+                a.push_back((a[i]%a[i+1]));
+                i++;
+            }
+        }
+        if(a==b) cout<<b[0]<<" "<<b[1];
+        else cout<<-1;
+        cout<<endl;        
+    }
+    return 0;
+}
